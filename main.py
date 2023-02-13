4ff66bd2dbfdec31f7524747f877d947f24cfe7e
Sefyupowa <mp.badr1995@gmail.com>
2023-02-13 22:12:20 +0100

    j

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#Le but du programme est d'effectué un programme pour jouer tic-tac-toe entre deux joueurs.
+#le joueur pourra choisir une ligne et une colonne pour affecter le caractère "x" ou "o"
+#la plupart des fonctionnalités sont codées, vous avez à compléter le code, ou utiliser les fonctions déjà faites.
 import random
 
 tableau = [['.', '.','.'],
@@ -82,15 +85,17 @@ def verifierLignes(tableau):
 
 
 def choisirCase(tableau, joueur , character):
+	#Demandez à l'utilisateur de choisir une line et une colonne.
 	ligne=int(input("Entrez le numero de la ligne : "))
 	colonne=int(input("Entrez le numero de la colone : "))
-
+	#verifiez que le la ligne et la colonne est bien un chiffre entre 1 et 3.
 	if ligne not in [1,2,3]:
 		print("impossible de choisir cette ligne ... :(")
 		choisirCase(tableau , joueur, character)
 	elif colonne not in [1,2,3]:
 		print("impossible de choisir cette colonne ... :(")
-
+	#Verifiez que la case n'est pas remplie. pour accéder à une case d'un tableau 
+	#vous utiliserz la syntaxe suivante :  variable = tableau[ligne-1][colonne-1]  
 	elif tableau[ligne-1][colonne-1] != '.':
 		print("impossible de choisir cette case ... :(")
 		choisirCase(tableau , joueur, character)
@@ -102,8 +107,10 @@ def jouer(tableau):
 	joueur = random.randint(1,2)
 	character = random.choice(["x" , "o"])
 	while True :
+		#Affichez le joueur en utilisant un print.
 		print("c'est le tours du joueur" , joueur)
 		print()
+		#Appelez la fonction pour afficher le tableau.
 		afficherTabeau(tableau)
 		choisirCase(tableau , joueur , character)
 		gagnant = verifierLignes(tableau)
@@ -117,12 +124,13 @@ def jouer(tableau):
 			afficherTabeau(tableau)
 			return
 
-
+		#Changez le tours du joueur
 		if joueur ==1:
 			joueur =2
 		else :
 			joueur =1
 
+		#Changez caractère
 		if character == "x":
 			character = "o"
 		else :
